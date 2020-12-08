44c99ee1d36205757caaa1e32b0485a3ad756581
yukty2000 <55875848+yukty2000@users.noreply.github.com>
2020-12-08 18:58:02 +0530

    Update and rename main.cpp to ModularExponentiationusingRecursion.cpp

---
+++ ModularExponentiationusingRecursion.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define N 1000000
+using namespace std;
+typedef long long int lli;
+typedef pair<int,int> pii;
+typedef tuple<int,int,int> tii;
+//modular exponentiation using recursion
+int ModM(int x,int n,int M)
+{
+    if(n==0)
+        return 1;
+    else if(n%2==0)
+    {
+        int y=ModM(x,n/2,M);
+        return (y*y)%M;
+    }
+    else
+    {
+        return ((x%M)*ModM(x,n-1,M))%M;
+    }
+}
+int main()
+{
+    cout<<ModM(3,5,1e5+7)<<endl;
+    return 0;
+}
