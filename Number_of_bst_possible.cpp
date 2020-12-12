2a47ea29339272969c696d00a5692200b4c0b3e2
yukty2000 <55875848+yukty2000@users.noreply.github.com>
2020-12-12 11:22:40 +0530

    Update Number_of_bst_possible.cpp

--- Number_of_bst_possible.cpp
+++ Number_of_bst_possible.cpp
@@ -1,28 +1,51 @@
+/* This program finds the number of binary search trees possible given all the node values in 
+    sorted order. As our input we are only provided with the length of this sequence = n.
+    
+    A tree is an acyclic graph . A binary tree is a tree where each node has 0, 1 or 2 children. 
+    A BST is a binary tree where the left child has value less than root and the right child has value greater than the root.  
+    
+    We use the dynamic programming approach where our root is varied from 1 to n and all the nodes having 
+    value less than root value are put in the left subtree and the nodes having value greater than root value 
+    are put in the right subtree. 
+    
+    Time Complexity - O(n^2)
+    Space Complexity - O(n)
+    
+    Note:- The approach is similar to finding the nth catalan number.
+    */
+
 #include <bits/stdc++.h>
 using namespace std;
-// Find the total number of binary search trees possible given n number of keys
-// O(n^2) complexity - Dynamic Programming Approach
-int main()
+
+typedef long long int lli;
+
+lli PossibleBST(int n)
 {
-    int n;
-    cin>>n;
-    lli *dp=(lli *)malloc((n+1)*sizeof(lli));
-    dp[0]=1,dp[1]=1;
-    //Since number of trees that can be formed for n=0/1 is 1
-    for(int i=2;i<=n;i++)
+    lli *Dp=(lli *)malloc((n+1)*sizeof(lli));
+    
+    Dp[0]=1,Dp[1]=1;                        //Since number of BST's that can be formed for n=0/1 is 1
+    
+    for(int i=2;i<=n;i++)                   //Calculating possible number of BSTs for all i<=n and storing them for future use-Tabulation Method
     {
-        dp[i]=0;
-        for(int j=0;j<i;j++)
+        Dp[i]=0;
+        for(int j=0;j<i;j++)                
         {
-            dp[i]+=(dp[j]*dp[i-j-1]);
+            Dp[i]+=(Dp[j]*Dp[i-j-1]);
         }
     }
-    //Calculation is same to that of catalan number
-    /*for(int i=0;i<=n;i++)
-        cout<<dp[i]<<" ";
-    cout<<endl;*/
-    cout<<dp[n]<<endl;
-    free(dp);
+    
+    lli ans=Dp[n];
+    
+    free(Dp);
+    
+    return ans;
+}
+
+int main()                                  //Driver function
+{
+    int n;                                  //Enter length of sequence
+    cin>>n;
+    cout<<PossibleBST(n)<<endl;
     return 0;
 }
 /* Sample input:-
