17d197f27fc6886a12d4c24117f8aa4093a92f67
yukty2000 <55875848+yukty2000@users.noreply.github.com>
2021-01-02 17:33:30 +0530

    Update DiceThrow.cpp

--- DiceThrow.cpp
+++ DiceThrow.cpp
@@ -1,7 +1,7 @@
 /* 
 Leetcode
 You have d dice, and each die has f faces numbered 1, 2, ..., f.
-Return the number of possible ways (out of fd total ways) modulo 10^9 + 7 to roll the dice so the sum of the face up numbers equals target.
+Return the number of possible ways (out of f^d total ways) modulo 10^9 + 7 to roll the dice so the sum of the face up numbers equals target.
 
 Input: d = 1, f = 6, target = 3
 Output: 1
